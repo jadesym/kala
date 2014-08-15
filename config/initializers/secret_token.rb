868432a2ff230abb0bff8b34653ecfb93a7d8bbe
Kevin Fu <kevin.fu808@gmail.com>
2014-08-14 20:18:22 -0500

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kala::Application.config.secret_key_base = '424973ad69f06eec0f8bf21f5287a33c990aab49322365decba964e818b58cf40db29346574c74d9007d29141b248a6a06131d56f8fc2c0975f654bd23fd8651'
